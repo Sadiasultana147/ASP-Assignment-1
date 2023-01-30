53aa29cd514ef8e11a15e309dd2707385681fc6f
Sadiasultana147 <sadiafency.dev@outlook.com>
2023-01-30 17:41:18 +0600

    1st Commit

---
+++ Assignment-1/Assignment-1/JsonFormatter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public class JsonFormatter
+    {
+        public static string Convert(object item)
+        {
+            //Create StringBuilder Object
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (item == null)
+            {
+                stringBuilder.Append(" \"\" ");
+
+            }
+            else
+            {
+                // Get Parameter's Data Type
+                Type type = item.GetType();
+
+                // Get Parameter's Properties Info
+                IEnumerable<PropertyInfo> propertyInfo = type.GetProperties();
+
+                //Check Data Type is Primitive or string
+                if (!item.GetType().IsPrimitive && item.GetType() != typeof(string))
+                {
+                    bool checkArray = typeof(IEnumerable).IsAssignableFrom(item.GetType());
+                    // Json Structure
+                    if (checkArray == true)
+                    {
+                        stringBuilder.Append($"[");
+                    }
+                    else
+                    {
+                        stringBuilder.Append($"{{");
+                    }
+                    if (checkArray == true)
+                    {
+                        foreach (var pInfo in propertyInfo)
+                            if (pInfo.GetType().IsPrimitive || pInfo.GetType() == typeof(string))
+                            {
+                                stringBuilder.Append($"\"{pInfo}\", ");
+                            }
+
+                            else
+                            {
+                                stringBuilder.Append($"\"{pInfo.Name}\": {Convert(pInfo.GetValue(item))}, ");
+                            }
+
+                    }
+                    else
+                    {
+                        foreach (var pInfo in propertyInfo)
+                        {
+                            if (pInfo.PropertyType.IsPrimitive || pInfo.PropertyType == typeof(string))
+                            {
+                                stringBuilder.Append($"\"{pInfo.Name}\": \"{pInfo.GetValue(item)}\", ");
+                            }
+
+                            else
+                            {
+                                stringBuilder.Append($"\"{pInfo.Name}\": {Convert(pInfo.GetValue(item))}, ");
+                            }
+                        }
+
+
+                    }
+
+                    stringBuilder.Remove(stringBuilder.ToString().Length - 2, 2);
+
+                    if (checkArray == true)
+                    {
+                        stringBuilder.Append($"]");
+                    }
+
+                    else
+                    {
+                        stringBuilder.Append($"}}");
+                    }
+                }
+
+                else
+                {
+                    stringBuilder.Append(item.ToString());
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Assignment-1/Assignment-1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.Json;
+using Assignment_1;
+
+namespace Assignment_1
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public Instructor Teacher { get; set; }
+        public List<Topic> Topics { get; set; }
+        public double Fees { get; set; }
+        public List<AdmissionTest> Tests { get; set; }
+    }
+    public class AdmissionTest
+    {
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public double TestFees { get; set; }
+    }
+    public class Topic
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Session> Sessions { get; set; }
+    }
+    public class Session
+    {
+        public int DurationInHour { get; set; }
+        public string LearningObjective { get; set; }
+    }
+    public class Instructor
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address PresentAddress { get; set; }
+        public Address PermanentAddress { get; set; }
+        public List<Phone> PhoneNumbers { get; set; }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+    public class Phone
+    {
+        public string Number { get; set; }
+        public string Extension { get; set; }
+        public string CountryCode { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+            course.Title = "Asp.net";
+
+            course.Fees = 30000;
+            course.Teacher = new Instructor()
+            {
+                Name = "Jalal"
+            };
+
+            var r = JsonFormatter.Convert(course);
+            Console.WriteLine(r);
+
+            Console.ReadLine();
+        }
+    }
+}
